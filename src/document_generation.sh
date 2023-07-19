3959614029fed805496f35b96b220158d709b907
ruiheesi <107956586+ruiheesi@users.noreply.github.com>
2023-07-19 15:47:29 -0400

    minor updates

--- src/document_generation.sh
+++ src/document_generation.sh
@@ -54,7 +54,7 @@ echo "import os" >> source/conf.py
 echo "import sys" >> source/conf.py
 echo "path = os.path.abspath('../../src')" >> source/conf.py
 echo "sys.path.insert(0,path)" >> source/conf.py
-echo "extensions = ['sphinx.ext.napoleon, 'sphinx.ext.autodoc'']" >> source/conf.py
+echo "extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc']" >> source/conf.py
 
 # Adding theme. Currentlu using
 # Read the Docs Sphinx Theme 
