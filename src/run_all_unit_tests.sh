935690438394214ae1484b6e5fc8761e1ba5a5e4
ruiheesi <107956586+ruiheesi@users.noreply.github.com>
2023-09-21 10:58:48 -0400

    Update error check methods

--- src/run_all_unit_tests.sh
+++ src/run_all_unit_tests.sh
@@ -26,16 +26,15 @@ do
   echo "====================================================================="
   echo -e "\nTesting: $test_to_run"
   
-  pytest $test_to_run
+  pytest_output=$(pytest $test_to_run 2>&1)
 
-  pytest_exit_status=$?
-
-  if [ $pytest_exit_status -eq 0 ]; then
-      echo -e "\nTest passed\n"
-      test_records+=("$test_to_run : Passed. ")
-  else
+  # Check if "ERROR" is in the output
+  if echo "$pytest_output" | grep -q "ERRORS"; then
       echo -e "\nTest failed\n"
       test_records+=("$test_to_run : Failed. ")
+  else
+      echo -e "\nTest passed\n"
+      test_records+=("$test_to_run : Passed. ")
   fi
 
   echo "====================================================================="
