3bce1738880287c1a21f80babe77fcfa4525dda2
ruiheesi <107956586+ruiheesi@users.noreply.github.com>
2023-12-18 09:52:13 -0500

    test: new page development method

--- src/document_generation.sh
+++ src/document_generation.sh
@@ -48,25 +48,53 @@ echo "import os" >> source/conf.py
 echo "import sys" >> source/conf.py
 echo "path = os.path.abspath('../../src')" >> source/conf.py
 echo "sys.path.insert(0,path)" >> source/conf.py
-echo "extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc', 'sphinx.ext.autosectionlabel']" >> source/conf.py
+echo "extensions = [
+	'sphinx.ext.napoleon',
+	'sphinx.ext.autodoc',
+	'sphinx.ext.autosectionlabel',
+	'sphinx.ext.todo',
+	'sphinx.ext.viewcode',
+	'sphinx.ext.githubpages']" >> source/conf.py
 sed -i "s/^html_theme = .*/html_theme = \"$theme\"/" source/conf.py
 echo "html_theme_options = {
     'collapse_navigation': False,
-    'navigation_depth': 4,
+    'navigation_depth': 3,
     'sticky_navigation': True,
     'titles_only': False,
     'style_external_links': True,
-    'style_nav_header_background': 'blue'
 }" >> source/conf.py
 
 ###################### Generalized Method #########################
 echo "Updating documentation..."
 sphinx-apidoc -f -o source ../src/spac
 
-sed -i 's/:maxdepth: 2/:maxdepth: 4/' source/index.rst
+# sed -i 's/:maxdepth: 2/:maxdepth: 3/' source/index.rst
 
-# Include README.md on the landing page
-echo ".. include:: ../../README.md" >> source/index.rst
+# # Include README.md on the landing page
+# echo ".. mdinclude:: ../../README.md" >> source/index.rst
+
+cat > source/index.rst <<EOF
+$name Documentation
+===============================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   modules
+
+Welcome to the $name's documentation!
+--------------------------------------
+
+.. mdinclude:: ../../README.md
+
+Indices and tables
+==================
+
+* :ref:\`genindex\`
+* :ref:\`modindex\`
+* :ref:\`search\`
+EOF
 
 echo "Generating html now..."
 make html
